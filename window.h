6cee8053037695c67464b86b1f3ca36b8ae13fdb
Mextrosity <thorfredriksson2022@gmail.com>
2024-11-29 18:30:17 +0100

    Add files via upload
    New stuff, might change or add later, for now on its like this, Enjoy the progress!

---
+++ window.h
@@ -0,0 +1,8 @@
+#ifndef WINDOW_H // Include guard to prevent multiple inclusions
+#define WINDOW_H
+
+void initializeWindow();    // Declare functions
+void handleWindowEvents();
+void destroyWindow();
+
+#endif // WINDOW_H
